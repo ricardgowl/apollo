d2b87b9ac8c93f65c3cf4ca61f1c271a997d36db
Ricardo Gonzalez <ricardo.gonzalez@wizeline.com>
2025-06-19 15:36:47 -0600

    Fix: Refactor and clean code

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
-const {ApolloServer, gql} = require('apollo-server')
+const {ApolloServer} = require('apollo-server')
 const SessionsAPI = require('./data_sources/sessions')
-//const sessions = require('./data/sessions.json')
 
-const typeDefs = gql`
-type Query {
-    sessions(
-    id: ID
-    title: String
-    description: String
-    startsAt: String
-    endsAt: String
-    room: String
-    day: String
-    format: String
-    track: String
-    level: String
-    ): [Session],
-    sessionById(id: ID): Session
-}
+const typeDefs = require('./schema.js')
 
-type Session {
-    id: ID!,
-    title: String!
-    description: String
-    startsAt: String
-    endsAt: String
-    room: String
-    day: String
-    format: String
-    track: String @deprecated(reason: "Use trackName instead")
-    level: String
-}`
-
-const resolvers = {
-    Query: {
-        sessions: (parent, args, {dataSources}, info) => {
-            return dataSources.sessionAPI.getSessions(args);
-        },
-        sessionById: (parent, {id}, {dataSources}, info) => {
-            return dataSources.sessionAPI.getSessionById(id);
-        }
-    }
-}
+const resolvers = require('./resolvers.js')
 
 const dataSources = () => ({
     sessionAPI: new SessionsAPI()
