e3f15366bcaf8ddfa0468b3dc9d7aa02731def4e
Ricardo Gonzalez <ricardo.gonzalez@wizeline.com>
2025-06-19 15:24:10 -0600

    Feat: Search by multiple columns

--- index.js
+++ index.js
@@ -4,21 +4,38 @@ const SessionsAPI = require('./data_sources/sessions')
 
 const typeDefs = gql`
 type Query {
-    sessions: [Session],
+    sessions(
+    id: ID
+    title: String
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+    level: String
+    ): [Session],
     sessionById(id: ID): Session
 }
 
 type Session {
     id: ID!,
     title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
     track: String @deprecated(reason: "Use trackName instead")
-    trackName: String
+    level: String
 }`
 
 const resolvers = {
     Query: {
         sessions: (parent, args, {dataSources}, info) => {
-            return dataSources.sessionAPI.getSessions();
+            return dataSources.sessionAPI.getSessions(args);
         },
         sessionById: (parent, {id}, {dataSources}, info) => {
             return dataSources.sessionAPI.getSessionById(id);
